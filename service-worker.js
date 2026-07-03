d46b35d40db8b8c94a335ce2eb5013a69b290691
tsfina-sys <tsfina@gmail.com>
2026-07-03 19:27:53 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const PZL_BUILD = "PZL_BASEMAP_PERSISTENCE_20260703D448";
+const PZL_BUILD = "PZL_BASEMAP_STABLE_20260703D449";
 const APP_CACHE = "pzl-app-" + PZL_BUILD;
 const TILE_CACHE = "pzl-map-tiles-v2";
 const CACHE_PREFIX = "pzl-app-";
