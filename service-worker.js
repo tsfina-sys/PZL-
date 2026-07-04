c36792824743abf8c28fdbd145e3b531db51c3e7
tsfina-sys <tsfina@gmail.com>
2026-07-04 12:44:17 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const PZL_BUILD = "PZL_POINT_NAMES_PINS_ROUTES_20260704D451";
+const PZL_BUILD = "PZL_FIRE_POINT_LIST_20260704D453";
 const APP_CACHE = "pzl-app-" + PZL_BUILD;
 const TILE_CACHE = "pzl-map-tiles-v2";
 const CACHE_PREFIX = "pzl-app-";
