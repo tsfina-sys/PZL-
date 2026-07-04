e3940ef4295e73e575e6adb6cfb8cbe4aaebc7bd
tsfina-sys <tsfina@gmail.com>
2026-07-04 14:44:21 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const PZL_BUILD = "PZL_FIRE_DELETE_MAP_CLEANUP_20260704D455";
+const PZL_BUILD = "PZL_VOICE_COMMANDS_V1_20260704D456";
 const APP_CACHE = "pzl-app-" + PZL_BUILD;
 const TILE_CACHE = "pzl-map-tiles-v2";
 const CACHE_PREFIX = "pzl-app-";
